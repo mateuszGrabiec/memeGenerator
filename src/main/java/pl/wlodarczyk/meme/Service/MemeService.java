a1a8cd48f09f156194302e9fe79160f93fd1240d
mati <mateuszgrabiec7@gmail.com>
2019-12-28 18:45:20 +0100

    choosing meme from list

--- src/main/java/pl/wlodarczyk/meme/Service/MemeService.java
+++ src/main/java/pl/wlodarczyk/meme/Service/MemeService.java
@@ -1,5 +1,35 @@
 package pl.wlodarczyk.meme.Service;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+import org.springframework.web.client.RestTemplate;
+import pl.wlodarczyk.meme.Model.Container;
+import pl.wlodarczyk.meme.Model.Meme;
+
+import java.util.List;
+
+@Service
 public class MemeService {
+    private List<Meme> memeList;
+
+    public MemeService() {
+        RestTemplate restTemplate = new RestTemplate();
+        Container container;
+        container = restTemplate.getForObject("https://api.imgflip.com/get_memes", Container.class);
+        this.memeList = container.getData().getMemes();
+    }
+
+    public List<Meme> getMemeUrl(){
+        return this.memeList;
+    }
+
+    public Meme getMemeById(String id) {
+        for (Meme meme : this.memeList) {
+            if (meme.getId().equals(id)) return meme;
+        }
+        return null;
+    }
 
 }
---
+++ src/main/java/pl/wlodarczyk/meme/controller/MemeController.java
@@ -0,0 +1,37 @@
+package pl.wlodarczyk.meme.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.wlodarczyk.meme.Model.Meme;
+import pl.wlodarczyk.meme.Service.MemeService;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+public class MemeController {
+    private MemeService memeService;
+
+    @Autowired
+    public MemeController(MemeService memeService) {
+        this.memeService = memeService;
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("memeList", memeService.getMemeUrl());
+        return "index";
+    }
+
+    @RequestMapping(value = "/this-meme", method = GET)
+    public String get(@RequestParam("name") String id, Model model) {
+        model.addAttribute("meme",memeService.getMemeById(id));
+        model.addAttribute("numOfBox",memeService.getMemeById(id).getBoxCount());
+        return "/edit";
+    }
+}
