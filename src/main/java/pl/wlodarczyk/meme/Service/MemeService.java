c0ad20a6b75a93cec0d9a91c5c2bb7e95cda44bc
mati <mateuszgrabiec7@gmail.com>
2020-01-07 18:03:33 +0100

    fix 1.03

--- src/main/java/pl/wlodarczyk/meme/Service/MemeService.java
+++ src/main/java/pl/wlodarczyk/meme/Service/MemeService.java
@@ -1,5 +1,6 @@
 package pl.wlodarczyk.meme.Service;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import pl.wlodarczyk.meme.Model.Meme;
 
 import java.util.List;
 
+@Component
 @Service
 public class MemeService {
     private List<Meme> memeList;
