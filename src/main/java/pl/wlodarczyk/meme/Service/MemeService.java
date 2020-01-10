3d3f09390516ba426ddb6edb15a979a2fbcf80e7
mati <mateuszgrabiec7@gmail.com>
2020-01-10 01:20:37 +0100

    fix 1.15 (reformat, better font, align, resizing)

--- src/main/java/pl/wlodarczyk/meme/Service/MemeService.java
+++ src/main/java/pl/wlodarczyk/meme/Service/MemeService.java
@@ -6,7 +6,6 @@ import org.springframework.web.client.RestTemplate;
 import pl.wlodarczyk.meme.Model.Container;
 import pl.wlodarczyk.meme.Model.Meme;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,19 +14,19 @@ public class MemeService {
 
     public List<Meme> getMemeUrl() {
 
-            RestTemplate rt = new RestTemplate();
-            HttpHeaders headers = new HttpHeaders();
-            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-            headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-            HttpEntity<Container> entity = new HttpEntity<>(headers);
-            String url = "https://api.imgflip.com/get_memes";
-            ResponseEntity<Container> res = rt.exchange(url, HttpMethod.GET, entity, Container.class);
-            if(res.getBody()!=null)return res.getBody().getData().getMemes();
-            else return null;
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        HttpEntity<Container> entity = new HttpEntity<>(headers);
+        String url = "https://api.imgflip.com/get_memes";
+        ResponseEntity<Container> res = rt.exchange(url, HttpMethod.GET, entity, Container.class);
+        if (res.getBody() != null) return res.getBody().getData().getMemes();
+        else return null;
     }
 
     public Meme getMemeById(String id) {
-        for (Meme meme :getMemeUrl()) {
+        for (Meme meme : getMemeUrl()) {
             if (meme.getId().equals(id)) return meme;
         }
         return null;
